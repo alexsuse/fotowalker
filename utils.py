b1af08ea72c5731ae5487372137c0f50b72d7d3b
alexsuse <alexsusemihl@gmail.com>
2014-05-15 17:38:26 +0200

    Using os.path, checking for dirs

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-from os.path import splitext, getmtime
+import os.path
+import os
 from datetime import datetime
+from shutil import copy2
 
 def newname(src):
     """
@@ -7,12 +9,24 @@ def newname(src):
     the date the file was created with the same extension
     ex: YY_MM_DD_HH_mm_SS.png
     """
-    filename, ext = splitext(src)
-    lm = datetime.fromtimestamp(getmtime(src))
+    filename, ext = os.path.splitext(src)
+    lm = datetime.fromtimestamp(os.path.getmtime(src))
     name = "_".join( map(str, [lm.year, lm.month, lm.day, 
                                lm.hour, lm.minute, lm.second]))
+    name = os.path.join(str(lm.year),str(lm.month),str(lm.day),name)
     return name + ext
+
+def copyfile(src, dst):
+    """
+    Copies a file from src to dst
+    creating new directories if needed.
+    """
+    if not os.path.exists(dst):
+        print "creating directories"
+        os.makedirs(dst)
+    copy2(src,dst)
 
 if __name__=="__main__":
     src = 'test.png'
     print newname(src)
+    copyfile(src,newname(src))
